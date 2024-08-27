3cada5dcb33d4036018f7b64d36ab9a447824ac7
ericekirem <irem.ericek@icloud.com>
2024-08-27 11:07:26 +0300

    Add project files.

---
+++ Inheritance/BaseKisi.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    class BaseKisi
+    {
+        public string Ad { get; set; }
+        public string Soyad { get; set; }
+
+        public void AdSoyadYazdir()
+        {
+            Console.WriteLine($"Ad: {Ad}, Soyad: {Soyad}");
+        }
+    }
+
+    class Ogrenci : BaseKisi
+    {
+        public string OgrenciNumarasi { get; set; }
+
+        public void OgrenciBilgileriniYazdir()
+        {
+            AdSoyadYazdir(); // Base sınıfındaki metodu çağırıyoruz
+            Console.WriteLine($"Öğrenci Numarası: {OgrenciNumarasi}");
+        }
+    }
+
+    class Ogretmen : BaseKisi
+    {
+        public decimal Maas { get; set; }
+
+        public void OgretmenBilgileriniYazdir()
+        {
+            AdSoyadYazdir(); // Base sınıfındaki metodu çağırıyoruz
+            var cultureInfo = new CultureInfo("tr-TR");
+            var numberFormatInfo = (NumberFormatInfo)cultureInfo.NumberFormat.Clone();
+            numberFormatInfo.CurrencySymbol = "TL";  // TL simgesini kullanıyoruz
+            Console.WriteLine($"Maaş: {Maas.ToString("C", numberFormatInfo)}");
+        }
+    }
+}
---
+++ Inheritance/Program.cs
@@ -0,0 +1,30 @@
+﻿using Inheritance;
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Öğrenci nesnesi oluşturma ve değer atama
+        Ogrenci ogrenci = new Ogrenci
+        {
+            Ad = "Mehmet",
+            Soyad = "Onat",
+            OgrenciNumarasi = "123456"
+        };
+
+        // Öğrenci bilgilerini yazdırma
+        ogrenci.OgrenciBilgileriniYazdir();
+
+        // Öğretmen nesnesi oluşturma ve değer atama
+        Ogretmen ogretmen = new Ogretmen
+        {
+            Ad = "Ali",
+            Soyad = "Yıldırım",
+            Maas = 8500
+        };
+
+        // Öğretmen bilgilerini yazdırma
+        ogretmen.OgretmenBilgileriniYazdir();
+    }
+}
